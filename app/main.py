e9126e8ce07ea284918be7c2a71069baf89039b7
tripleaceinnovations <dexy004@gmail.com>
2022-07-03 10:10:01 -0400

    fix: pylint fixes

--- app/main.py
+++ app/main.py
@@ -10,21 +10,21 @@ app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
+  name: str
+  price: float
+  is_offer: bool = None
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+  return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+  return {"item_id": item_id, "q": q}
 
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+  return {"item_name": item.name, "item_id": item_id}
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from app.main import app
 @pytest.fixture(scope="function")
 def testclient():
 
-    with TestClient(app) as client:
-        # Application 'startup' handlers are called on entering the block.
-        yield client
-    # Application 'shutdown' handlers are called on exiting the block.
+  with TestClient(app) as client:
+      # Application 'startup' handlers are called on entering the block.
+      yield client
+  # Application 'shutdown' handlers are called on exiting the block.
